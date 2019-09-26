0054af9591e8b6cbc2fb4cd18b4bb76630fe6182
Alex <a.khatuntsev2015@gmail.com>
2019-09-26 14:26:05 +0300

    delete some comments

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,6 @@ import './index.css'
 import App from './App'
 import store from './redux/redux-store'
 
-
 ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
@@ -18,7 +17,4 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister()
